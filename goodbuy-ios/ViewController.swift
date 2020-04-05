f62f9cf62ac0bcec22c002e7fd70d78aeb634c45
Anthony <anthony.sherrill@code.berlin>
2020-04-05 19:06:24 +0200

    Initial Commit

---
+++ goodbuy-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goodbuy-ios
+//
+//  Created by Anthony Sherrill on 05.04.20.
+//  Copyright © 2020 Anthony Sherrill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
